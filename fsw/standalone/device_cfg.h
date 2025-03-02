bd375dbfccddd1fca20196e9e540f7c51a785e76
Jack Connor Vanlyssel <jackvanlyssel@gmail.com>
2025-03-01 17:35:38 -0700

    chnage

--- fsw/standalone/device_cfg.h
+++ fsw/standalone/device_cfg.h
@@ -7,7 +7,7 @@
 #define CLIENT_CFG
 /* Note: NOS3 uart requires matching handle and bus number */
 #define CLIENT_CFG_STRING           "/dev/usart_16"
-#define CLIENT_CFG_HANDLE           16 
+#define CLIENT_CFG_HANDLE           16
 #define CLIENT_CFG_BAUDRATE_HZ      115200
 #define CLIENT_CFG_MS_TIMEOUT       250
 #define CLIENT_CFG_DEBUG
